c62e2ed08a8eb4897154fd06d497352ccd7c1a1a
bilinc PC <bilinc@kth.se>
2021-09-01 21:53:28 +0200

    Created class to demonstrate Getters and Setters.

---
+++ src/chapter4/ElectricGuitar.java
@@ -0,0 +1,32 @@
+package chapter4;
+
+public class ElectricGuitar {
+
+    String brand;
+    int numOfPickups;
+    boolean rockStarUsesIt;
+
+    String getBrand() {
+        return brand;
+    }
+
+    void setBrand(String aBrand) {
+        brand = aBrand;
+    }
+
+    int getNumOfPickups() {
+        return numOfPickups;
+    }
+
+    void setNumOfPickups(int num) {
+        numOfPickups = num;
+    }
+
+    boolean getRockStarUsesIt() {
+        return rockStarUsesIt;
+    }
+
+    void setRockStarUsesIt(boolean yesOrNo) {
+        rockStarUsesIt = yesOrNo;
+    }
+}
