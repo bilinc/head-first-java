4c3b5fc66ebe18f676bb93e9b89da1f7f4c88070
bilinc PC <bilinc@kth.se>
2021-09-03 22:49:14 +0200

    Solved mixed message, ch4

--- src/chapter4/Mix4.java
+++ src/chapter4/Mix4.java
@@ -6,16 +6,17 @@ public class Mix4 {
     int count = 0;
     Mix4[] m4a = new Mix4[20];
     int x = 0;
-    while (x < 9) {
+    while (x < 7) {   // x < 9, x < 20, x < 7, x < 19
       m4a[x] = new Mix4();
       m4a[x].counter = m4a[x].counter + 1;
       count = count + 1;
       count = count + m4a[x].maybeNew(x);
       x += 1;
     }
+    System.out.println(count + " " + m4a[1].counter);
   }
   public int maybeNew(int index) {
-    if (index < 5) {
+    if (index < 7) {    // index < 5, index < 5, index < 7, index < 1
       Mix4 m4 = new Mix4();
       m4.counter = m4.counter + 1;
       return 1;
