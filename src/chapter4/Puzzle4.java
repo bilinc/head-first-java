fdb2055ab621cacc51d37ac827582d81b56d7f47
bilinc PC <bilinc@kth.se>
2021-09-05 23:41:26 +0200

    Initiated puzzle in chapter 4.

---
+++ src/chapter4/Puzzle4.java
@@ -0,0 +1,22 @@
+package chapter4;
+
+public class Puzzle4 {
+    public static void main(String [] args) {
+        //
+        int y = 1;
+        int x = 0;
+        int result = 0;
+        while (x < 6) {
+            //
+            //
+            y = y * 10;
+            // x = x + 1;
+        }
+        x = 6;
+        while (x > 0) {
+            x = x - 1;
+            result = result + //
+        }
+        System.out.println("result " + result);
+    }
+}
---
+++ src/chapter4/Puzzle4b.java
@@ -0,0 +1,13 @@
+package chapter4;
+
+public class Puzzle4b {
+    int ivar;
+    public int doStuff(int factor) {
+        if (ivar > 100) {
+            return //
+        } else {
+            return //
+        }
+    }
+
+}
