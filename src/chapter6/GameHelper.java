7eba52a165533bdeaf4145e05c8f10a2aacec000
bilinc PC <bilinc@kth.se>
2022-05-08 12:54:46 +0200

    Updates

--- src/chapter6/GameHelper.java
+++ src/chapter6/GameHelper.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 
 public class GameHelper {
 
-    private static final String aplhabet = "abcdefg";
+    private static final String alphabet = "abcdefg";
     private int gridLength = 7;
     private int gridSize = 49;
     private int [] grid = new int[gridSize];
@@ -25,7 +25,59 @@ public class GameHelper {
     }
 
     public ArrayList<String> placeDotCom(int comSize) {
+      ArrayList<String> alphaCells = new ArrayList<>();
+      
+      String temp = null;
+      int [] coords = new int [comSize];
+      int attempts = 0;
+      boolean success = false;
+      int location = 0;
+      
+      comCount++;
+      int incr = 1;
+      if ((comCount % 2) == 1) {
+        incr = gridLength;
+      }
+      while ( !success & attempts++ < 200 ) {
+        location = (int) (Math.random() * gridSize);
+        // System.out.println("try " + location);
+        int x = 0;
+        success = true;
+        while (success && x < comSize) {
+          if (grid[location] == 0) {
+            coords[x++] = location;
+            location += incr;
+            if (location >= gridSize) {
+              success = false;
+            }
+            if (x>0 && (location % gridLength == 0)) {
+              success = false;
+            }
+          } else {
+            // System.out.println(" used " + location);
+            success = false;
+          }
+        }
+      }
+      
+      int x = 0;
+      int row = 0;
+      int column = 0;
+      // System.out.println("\n");
+      
+      while (x < comSize) {
+        grid[coords[x]] = 1;
+        row = (int) (coords[x] / gridLength);
+        column = coords[x] & gridLength;
+        temp = String.valueOf(alphabet.charAt(column));
+        
+        alphaCells.add(temp.concat(Integer.toString(row)));
+        x++;
+        // System.out.println(" coord "+x+" = " + alphaCells.get(x-1));
+      }
+      // System.out.println("\n");
 
-        return
+        return alphaCells;
+        
     }
 }
